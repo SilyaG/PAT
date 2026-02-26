51e7d9be1ef9f252e2df4aaf4f082c7f7414d127
Prima Olivier <oprima@univ-lyon2.fr>
2026-02-26 09:48:35 +0100

    permiere symbologie

--- Application_Cartographique/app.R
+++ Application_Cartographique/app.R
@@ -78,9 +78,8 @@ ui <- fluidPage(
   
   # Contenu principal
   tags$main(
-    class = "fr-container",
-    br(),
-    leafletOutput("map", height = "800px")
+    style = "padding: 0;",
+    leafletOutput("map", height = "85vh", width = "100%")
   ),
   
   # Footer officiel
@@ -108,16 +107,46 @@ server <- function(input, output, session) {
     xmax <- unname(bbox["xmax"])
     ymax <- unname(bbox["ymax"])
     
+    #Palettes de couleur
+    #Palette PAT
+    pal_pat <- colorFactor(
+      palette = c("#fbe769", "#E4794A"),
+      domain = couche_pat_4326$niveau
+    )
+    
+    # Centroïdes des communes
+    communes_centroid <- st_centroid(commune_aura)
+    
+    # Variable utilisée pour les communes
+    pop_com <- communes_centroid$population
+    
+    rayon_brut <- sqrt(pop_com)
+    
+    # Rayon proportionnel (mise à l'échelle)
+    rayon <- scales::rescale(rayon_brut, to = c(1, 50))
+    
     #Limitation du dézoom
     leaflet(
       options = leafletOptions(
-        minZoom = 8,
-        maxZoom = 11
+        minZoom = 6,
+        maxZoom = 15
       )
     ) %>%
       addProviderTiles("OpenStreetMap", group = "OSM") %>%
       fitBounds(xmin, ymin, xmax, ymax) %>%
       setMaxBounds(xmin, ymin, xmax, ymax) %>%
+      
+      # Plan IGN
+      addWMSTiles(
+        baseUrl = "https://data.geopf.fr/wms-r/wms",
+        layers  = "GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2",
+        options = WMSTileOptions(
+          format = "image/png",
+          transparent = TRUE,
+          version = "1.3.0"
+        ),
+        group = "Plan IGN"
+      ) %>%
       
       # RGA landuse
       addWMSTiles(
@@ -134,21 +163,18 @@ server <- function(input, output, session) {
       # Departement AURA
       addPolygons(
         data = dep_aura_4326,
-        color = "black",
-        weight = 1,
+        color = "#7b7b7b",
+        weight = 2,
         fillColor = NA,
         fillOpacity = 0,
         popup = ~paste(nom_officiel, sep = "<br/>"),
         group = "Departement"
       ) %>%
       
-      
-      
-      
       # Commune AURA
       addPolygons(
         data = commune_aura,
-        color = "black",
+        color = "#929292",
         weight = 1,
         fillColor = NA,
         fillOpacity = 0,
@@ -156,12 +182,27 @@ server <- function(input, output, session) {
         group = "Communes"
       ) %>%
       
+      # Cercle population
+      addCircleMarkers(
+        data = communes_centroid,
+        radius = rayon,
+        fillColor = "#CE614A",
+        color = "#ffffff",
+        weight = 1,
+        fillOpacity = 0.7,
+        popup = ~paste(
+          "<strong>", nom_officiel, "</strong><br/>",
+          "Valeur :", population
+        ),
+        group = "Indicateur communal"
+      ) %>%
+      
       # couche CLS
       addPolygons(
         data = couche_cls_4326,
-        color = "white",
+        color = "#869ECE",
         weight = 2,
-        fillOpacity = 0.4,
+        fillOpacity = 0.7,
         popup = ~paste(Nom_CLS, sep= "<br/>"),
         group = "Contrats locaux de santé"
       ) %>%
@@ -169,16 +210,17 @@ server <- function(input, output, session) {
       # couche Pat
       addPolygons(
         data = couche_pat_4326,
-        color = "orange",
-        weight = 2,
-        fillOpacity = 0.4,
+        color = ~pal_pat(niveau),
+        fillColor = ~pal_pat(niveau),
+        weight = 3,
+        fillOpacity = 0.35,
         popup = ~paste(nom_du_pat,niveau,pop_hab, sep= "<br/>"),
         group = "Projet Alimentaire Territoriaux"
       ) %>%
       
       # Menu couche
       addLayersControl(
-        overlayGroups = c("Projet Alimentaire Territoriaux","Contrats locaux de santé","Communes","Departement","Registre Parcellaire Graphique"),
+        overlayGroups = c("Projet Alimentaire Territoriaux","Contrats locaux de santé","Communes","Departement","Registre Parcellaire Graphique","Plan IGN","Indicateur communal"),
         options = layersControlOptions(collapsed = FALSE)
       )
   })
@@ -186,10 +228,3 @@ server <- function(input, output, session) {
 
 #
 shinyApp(ui, server)
-
-
-
-
-
-
-
