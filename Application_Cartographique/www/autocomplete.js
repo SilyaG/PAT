1bee39a130d2e080fcf74c3ba8ac3a0973e7d4d9
Guerboub Silya <sguerboub@univ-lyon2.fr>
2026-03-05 13:09:31 +0100

    commentaires dans les nouveaux fichier js et css

--- Application_Cartographique/www/autocomplete.js
+++ Application_Cartographique/www/autocomplete.js
@@ -1,17 +1,23 @@
+// Chacun des fichier JS correspond à une fonction bien défini compréhensible par le nom du fichier 
+// Ici il s'agit de la fonction d'autocompletion de la barre de recherche 
+
 document.addEventListener('DOMContentLoaded', function () {
 
-    var input  = document.getElementById('nom_du_pat'); // (2e) Entrer de recherche
-    var panel  = document.getElementById('autocomplete_panel'); // (2e) Recupere les éléments du panneau déroulant
-    var ulCom  = document.getElementById('suggest_communes'); // (2e) Liste des communes issues du champs communes 
-    var ulPat  = document.getElementById('suggest_pats'); // (2e) Liste des pat issus du champ nom_du_pat
-    var btn    = document.getElementById('search_button'); // (2e) Bouton rechercher
+// Recupération des éléments HTML nécessaire à la fonction
+    var input  = document.getElementById('nom_du_pat'); //Entrer de recherche
+    var panel  = document.getElementById('autocomplete_panel'); //Recupere les éléments du panneau déroulant
+    var ulCom  = document.getElementById('suggest_communes'); //Liste des communes issues du champs communes 
+    var ulPat  = document.getElementById('suggest_pats'); //Liste des pat issus du champ nom_du_pat
+    var btn    = document.getElementById('search_button'); //Bouton rechercher
 
+// securité : si un élément est manquant le script s'arrete
     if (!input || !panel || !ulCom || !ulPat || !btn) return;
 
-    var COMMUNES = window.AUTOCOMPLETE_COMMUNES || [];  // (2e) Variable table communes 
-    var PATS     = window.AUTOCOMPLETE_PATS || [];  // (2e) Variable table PAT
+// données utile pour l'autocompletion
+    var COMMUNES = window.AUTOCOMPLETE_COMMUNES || [];  //Variable table communes 
+    var PATS     = window.AUTOCOMPLETE_PATS || [];  //Variable table PAT
 
-    // Fonction de normalisation 
+    // Fonction de normalisation du texte, serte à comparer le texte sans être sensible à la casse
     function norm(s) {
       return (s || '')
         .trim() // supprime les espaces
@@ -23,8 +29,10 @@ document.addEventListener('DOMContentLoaded', function () {
         .replace(/\\s+/g, '');              // supprime tous les espaces
     }
 
+// vide le contenu d'une liste HTML
     function clearList(ul) { ul.innerHTML = ''; }
 
+// Ajout d'un élément dans la liste de suggestion
     function addItem(ul, label, clickable) {
       var li = document.createElement('li');
       li.textContent = label;
@@ -39,6 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
             Shiny.setInputValue('nom_du_pat', label, { priority: 'event' });
           }
 
+          // ferme le paneau d'autocompletion
           panel.style.display = 'none';
 
           // Lance la recherche après que Shiny ait bien reçu la valeur
@@ -54,10 +63,11 @@ document.addEventListener('DOMContentLoaded', function () {
       ul.appendChild(li);
     }
 
-// fonction de mise a jour suivant entrée 
+// fonction de mise a jour du panneau d'aucompletion 
     function updatePanel() {
       var q = norm(input.value);  // Récupere la saisie de l'utilisateur
 
+      // si le champs est vide le panneau est masqué 
       if (q.length === 0) {
         panel.style.display = 'none';
         clearList(ulCom);
@@ -71,19 +81,25 @@ document.addEventListener('DOMContentLoaded', function () {
 // Filtrage saisie PAT contient x
       var patMatches = PATS.filter(function(x){ return norm(x).includes(q); }).slice(0, 20);
 
-// Affichage des nouvelles listes
+// Affichage des resultats des filtres
       clearList(ulCom);
       clearList(ulPat);
 
+      // si aucune commune trouvé
       if (comMatches.length === 0) addItem(ulCom, 'Aucun résultat', false);
+
+      // ajout des communes trouvées
       for (var i = 0; i < comMatches.length; i++) addItem(ulCom, comMatches[i], true);
 
+      // si aucun PAT est trouvé
       if (patMatches.length === 0) addItem(ulPat, 'Aucun résultat', false);
       for (var j = 0; j < patMatches.length; j++) addItem(ulPat, patMatches[j], true);
 
+      // affiche le panneau d'autocompletion
       panel.style.display = 'block';
     }
 
+    // mise à jour des suggestion à chaque frappe
     input.addEventListener('input', updatePanel);
 
 // Entrée = recherche 
